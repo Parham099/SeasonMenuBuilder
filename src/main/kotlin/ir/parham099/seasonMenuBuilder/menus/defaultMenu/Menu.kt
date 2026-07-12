985f3f0fb4559616a10c6539c5c6101cade14b53
Parham <148459059+Parham099@users.noreply.github.com>
2026-07-12 20:12:43 +0330

    0.0.1-Beta

---
+++ src/main/kotlin/ir/parham099/seasonMenuBuilder/menus/defaultMenu/Menu.kt
@@ -0,0 +1,190 @@
+package ir.parham099.seasonMenuBuilder.menus.defaultMenu
+
+import ir.parham099.seasonMenuBuilder.menus.MenuBuilder
+import ir.parham099.seasonMenuBuilder.menus.MenuType
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.minimessage.MiniMessage
+
+fun menu(
+    title: Component,
+    size: Int,
+    menuType: MenuType = MenuType.DYNAMIC,
+    block: MenuBuilder.() -> Unit = {}
+): MenuBuilder {
+    return MenuBuilder(
+        title = title,
+        size = size,
+        menuType = menuType,
+        block = block
+    ).apply(block)
+}
+
+fun menu(
+    title: String,
+    size: Int,
+    menuType: MenuType = MenuType.DYNAMIC,
+    block: MenuBuilder.() -> Unit = {}
+): MenuBuilder {
+    return menu(
+        title = MiniMessage.miniMessage().deserialize(title),
+        size = size,
+        menuType = menuType,
+        block = block
+    ).apply(block)
+}
+
+fun menu9(
+    title: Component,
+    menuType: MenuType = MenuType.DYNAMIC,
+    block: MenuBuilder.() -> Unit = {}
+): MenuBuilder {
+    return menu(
+        title = title,
+        size = 9,
+        menuType = menuType,
+        block = block
+    )
+}
+
+fun menu9(
+    title: String,
+    menuType: MenuType = MenuType.DYNAMIC,
+    block: MenuBuilder.() -> Unit = {}
+): MenuBuilder {
+    return menu(
+        title = title,
+        size = 9,
+        menuType = menuType,
+        block = block
+    )
+}
+
+fun menu18(
+    title: Component,
+    menuType: MenuType = MenuType.DYNAMIC,
+    block: MenuBuilder.() -> Unit = {}
+): MenuBuilder {
+    return menu(
+        title = title,
+        size = 18,
+        menuType = menuType,
+        block = block
+    )
+}
+
+fun menu18(
+    title: String,
+    menuType: MenuType = MenuType.DYNAMIC,
+    block: MenuBuilder.() -> Unit = {}
+): MenuBuilder {
+    return menu(
+        title = title,
+        size = 18,
+        menuType = menuType,
+        block = block
+    )
+}
+
+fun menu27(
+    title: Component,
+    menuType: MenuType = MenuType.DYNAMIC,
+    block: MenuBuilder.() -> Unit = {}
+): MenuBuilder {
+    return menu(
+        title = title,
+        size = 27,
+        menuType = menuType,
+        block = block
+    )
+}
+
+fun menu27(
+    title: String,
+    menuType: MenuType = MenuType.DYNAMIC,
+    block: MenuBuilder.() -> Unit = {}
+): MenuBuilder {
+    return menu(
+        title = title,
+        size = 27,
+        menuType = menuType,
+        block = block
+    )
+}
+
+fun menu36(
+    title: Component,
+    menuType: MenuType = MenuType.DYNAMIC,
+    block: MenuBuilder.() -> Unit = {}
+): MenuBuilder {
+    return menu(
+        title = title,
+        size = 36,
+        menuType = menuType,
+        block = block
+    )
+}
+
+fun menu36(
+    title: String,
+    menuType: MenuType = MenuType.DYNAMIC,
+    block: MenuBuilder.() -> Unit = {}
+): MenuBuilder {
+    return menu(
+        title = title,
+        size = 36,
+        menuType = menuType,
+        block = block
+    )
+}
+
+fun menu45(
+    title: Component,
+    menuType: MenuType = MenuType.DYNAMIC,
+    block: MenuBuilder.() -> Unit = {}
+): MenuBuilder {
+    return menu(
+        title = title,
+        size = 45,
+        menuType = menuType,
+        block = block
+    )
+}
+
+fun menu45(
+    title: String,
+    menuType: MenuType = MenuType.DYNAMIC,
+    block: MenuBuilder.() -> Unit = {}
+): MenuBuilder {
+    return menu(
+        title = title,
+        size = 45,
+        menuType = menuType,
+        block = block
+    )
+}
+
+fun menu54(
+    title: Component,
+    menuType: MenuType = MenuType.DYNAMIC,
+    block: MenuBuilder.() -> Unit = {}
+): MenuBuilder {
+    return menu(
+        title = title,
+        size = 54,
+        menuType = menuType,
+        block = block
+    )
+}
+
+fun menu54(
+    title: String,
+    menuType: MenuType = MenuType.DYNAMIC,
+    block: MenuBuilder.() -> Unit = {}
+): MenuBuilder {
+    return menu(
+        title = title,
+        size = 54,
+        menuType = menuType,
+        block = block
+    )
+}
