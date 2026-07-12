b2506cfc6c56eb9bee5f3d41409bd0a71dc239d6
Parham <148459059+Parham099@users.noreply.github.com>
2026-07-12 20:10:48 +0330

    Delete SeasonMenuBuilder directory

--- SeasonMenuBuilder/src/main/kotlin/ir/parham099/seasonMenuBuilder/MenuDsl.kt
+++
@@ -1,4 +0,0 @@
-package ir.parham099.seasonMenuBuilder
-
-@DslMarker
-annotation class MenuDsl()
--- SeasonMenuBuilder/src/main/kotlin/ir/parham099/seasonMenuBuilder/menus/MenuType.kt
+++
@@ -1,6 +0,0 @@
-package ir.parham099.seasonMenuBuilder.menus
-
-enum class MenuType {
-    DYNAMIC,
-    STATIC
-}
